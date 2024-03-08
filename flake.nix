912a05e0c849c7fce08978b3d3c2a260ad9b4713
kix300 <kixwalkiki@gmail.com>
2024-03-08 12:32:32 +0100

    Dirty try

--- flake.nix
+++ flake.nix
@@ -5,17 +5,26 @@
     # NixOS official package source, using the nixos-23.11 branch here
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, ... }@inputs: {
-    # Please replace my-nixos with your hostname
+  outputs = { self, nixpkgs, nixos-hardware, home-manager, ... }@inputs: {
     nixosConfigurations.OzenOs = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
         ./configuration.nix
         nixos-hardware.nixosModules.asus-zephyrus-ga401
+
+	home-manager.nixosModules.home-manager
+	{
+	  home-manager.useGlobalPkgs = true;
+	  home-manager.useUserPackages = true;
+
+	  home-manager.users.ozen = import ./home.nix;
+        }	  
       ];
     };
   };
---
+++ home.nix
@@ -0,0 +1,40 @@
+{config, pkgs, ...}:
+
+{
+  home.username = "ozen";
+  home.homeDirectory = "home/ozen"
+
+  home.packages = with pkgs; [
+    neofetch
+  ];
+
+
+  programs.git = {
+
+     enable = true;
+     userName = "kix300";
+     userEmail = "kixwalkiki@gmail.com";
+
+  };
+
+  programs.starship = {
+     enable = true;
+
+     settings = {
+       add_newline = false;
+       aws.disabled = true;
+       gcloud.disabled = true;
+       line_break.disabled = true;
+     };
+  };
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+  };
+
+  home.stateVersion = "23.11";
+
+  programs.home-manager.enable = true;
+
+}
