5cd68f69563faf55013d9162259dd5be51b58d85
kix300 <kixwalkiki@gmail.com>
2025-01-19 23:55:25 +0100

    plugins nvim

--- nixvim/nixvim.nix
+++ nixvim/nixvim.nix
@@ -1,77 +1,90 @@
 { config, lib, pkgs, ... }:
 
 {
-  home.sessionVariables = {
-    EDITOR = "nvim";
-  };
+	home.sessionVariables = {
+		EDITOR = "nvim";
+	};
 
-  programs.neovim = {
-    defaultEditor = true;
-    enable = true;
-    viAlias = true;
-    vimAlias = true;
-    vimdiffAlias = true;
-    extraPackages = with pkgs; [
-      # Telescope
-      ripgrep
-    ];
+	programs.neovim = {
+		defaultEditor = true;
+		enable = true;
+		viAlias = true;
+		vimAlias = true;
+		vimdiffAlias = true;
+		extraPackages = with pkgs; [
+			ripgrep
+		];
 
-    plugins = with pkgs.vimPlugins; [
-      LazyVim
-      lazy-nvim
-	    treesitter-nvim
-      snacks-nvim
-      telescope-nvim
-    ];
- extraLuaConfig = ''
-      vim.g.mapleader = " " -- Need to set leader before lazy for correct keybindings
-      require("lazy").setup({
-        spec = {
-          -- add LazyVim and import its plugins
-          { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-          -- import/override with your plugins
-          { import = "plugins" },
-        },
-        dev = {
-          path = "${pkgs.vimUtils.packDir config.programs.neovim.finalPackage.passthru.packpathDirs}/pack/myNeovimPackages/start",
-          patterns = {""}, -- Specify that all of our plugins will use the dev dir. Empty string is a wildcard!
-        },
-        performance = {
-          -- Used for NixOS
-          reset_packpath = false,
-          rtp = {
-            reset = false,
-            disabled_plugins = {
-              "gzip",
-              -- "matchit",
-              -- "matchparen",
-              -- "netrwPlugin",
-              "tarPlugin",
-              "tohtml",
-              "tutor",
-              "zipPlugin",
-            },
-          }
-        },
-        defaults = {
-          -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-          -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-          lazy = false,
-          -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-          -- have outdated releases, which may break your Neovim install.
-          version = false, -- always use the latest git commit
-          -- version = "*", -- try installing the latest stable version for plugins that support semver
-        },
-        install = {
-          -- Safeguard in case we forget to install a plugin with Nix
-          missing = false,
-        },
-      })
-    '';
-  };
+		plugins = with pkgs.vimPlugins; [
+			LazyVim
+				lazy-nvim
+				nvim-treesitter
+				snacks-nvim
+				telescope-nvim
+				neo-tree-nvim
+				persistence-nvim
+				nvim-ts-autotag
+				nvim-lint
+				gitsigns-nvim
+				todo-comments-nvim
+				nvim-lspconfig
+				mini-ai
+				lualine-nvim
+				which-key-nvim
+				mini-pairs
+				mason-nvim
+				mason-lspconfig-nvim
+				bufferline-nvim
+				flash-nvim
+				marks-nvim
+				nvim-treesitter-textobjects
+				noice-nvim
+				tokyonight-nvim
+				catppucin-nvim
+				ts-comments-nvim
+				];
+		extraLuaConfig = ''
+			require("lazy").setup({
+					spec = {
+					{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
+					{ import = "plugins" },
+					},
+					dev = {
+					path = "${pkgs.vimUtils.packDir config.programs.neovim.finalPackage.passthru.packpathDirs}/pack/myNeovimPackages/start",
+					patterns = {""}, -- Specify that all of our plugins will use the dev dir. Empty string is a wildcard!
+					},
+					performance = {
+					-- Used for NixOS
+					reset_packpath = false,
+					rtp = {
+					reset = false,
+					disabled_plugins = {
+					"gzip",
+					-- "matchit",
+					-- "matchparen",
+					-- "netrwPlugin",
+					"tarPlugin",
+					"tohtml",
+					"tutor",
+					"zipPlugin",
+					},
+					}
+					},
+					defaults = {
+						lazy = true,
+						version = false, -- always use the latest git commit
+							-- version = "*", -- try installing the latest stable version for plugins that support semver
+					},
+					install = {
+						-- Safeguard in case we forget to install a plugin with Nix
+							missing = false,
+					},
+			})
+		'';
+	};
 
-  xdg.configFile."nvim/lua" = {
-    recursive = true;
-    source = ./lua;
-  };
+	xdg.configFile."nvim/lua" = {
+		recursive = true;
+		source = ./lua;
+	};
 }
