248bb8fd7b0066abf5482594399a018136387eb0
kix300 <kixwalkiki@gmail.com>
2025-05-16 19:57:50 +0200

    orlane: remove home manager

--- commons/configuration.nix
+++ commons/configuration.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
 	imports = [
@@ -16,6 +16,15 @@
 		hyprland.portalPackage = true;
 		hyprland.xwayland.enable = true;
 		ssh.startAgent = true;
+		bash = {
+			interactiveShellInit = ''
+				if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+					then
+						shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+						exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+						fi
+			'';
+		};
 		nh = {
 			enable = true;
 			clean.enable = true;
