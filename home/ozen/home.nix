00d6aa204050c3ccff16ef301e7393a82f0da123
kix300 <kixwalkiki@gmail.com>
2025-08-18 14:47:42 +0200

    conf zed good

--- home/ozen/home.nix
+++ home/ozen/home.nix
@@ -118,13 +118,7 @@ run ~/.dotfiles/home/commons/tmux/plugins/catppuccin/tmux/catppuccin.tmux
 				## tell zed to use direnv and direnv can use a flake.nix enviroment.
 				load_direnv = "shell_hook";
 				base_keymap = "VSCode";
-				theme = {
-					mode = "system";
-					light = "One Light";
-					dark = "One Dark";
-				};
 				show_whitespaces = "all" ;
-				ui_font_size = 16;
 
 			};
 
