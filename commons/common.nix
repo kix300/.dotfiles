d5a1f4fb6dc64660730447b4872a2335c4a7f468
kix300 <kixwalkiki@gmail.com>
2025-09-22 22:43:23 +0200

    Feat add rust analyzer

--- commons/common.nix
+++ commons/common.nix
@@ -1,99 +1,98 @@
-
 { pkgs, ... }:
 
 {
-	services = {
-		udev.enable = true;
-		gvfs.enable = true;
-		supergfxd.enable = true;
-		printing.enable = true;
-		upower.enable = true;
-		blueman.enable = true;
-		pipewire = {
-			enable = true;
-			alsa.enable = true;
-			alsa.support32Bit = true;
-			pulse.enable = true;
-		};
-		desktopManager.gnome.enable = true;
-  		libinput.enable = true;
-		displayManager.gdm.enable = true;
-		xserver = {
-			enable = true;
-			xkb = {
-				layout = "us";
-				variant = "";
-			};
-		};
-	};
-	hardware = {
-		i2c.enable = false;
-		bluetooth.enable = true;
-		bluetooth.powerOnBoot = true;
-		graphics = {
-			enable = true;
-		};
-	};
+  services = {
+    udev.enable = true;
+    gvfs.enable = true;
+    supergfxd.enable = true;
+    printing.enable = true;
+    upower.enable = true;
+    blueman.enable = true;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+    desktopManager.gnome.enable = true;
+    libinput.enable = true;
+    displayManager.gdm.enable = true;
+    xserver = {
+      enable = true;
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
+    };
+  };
+  hardware = {
+    i2c.enable = false;
+    bluetooth.enable = true;
+    bluetooth.powerOnBoot = true;
+    graphics = {
+      enable = true;
+    };
+  };
 
-	programs = {
-		dconf.enable = true;
-		xfconf.enable = true;
-	};
+  programs = {
+    dconf.enable = true;
+    xfconf.enable = true;
+  };
 
-	environment.systemPackages = with pkgs; [
-		vim
-		fish
-		clang
-		clang-tools
-		git
-		gnumake
-		ghostty
-		grim
-		iwgtk
-		libbsd
-		libclang
-		libgcc
-		libgccjit
-		lutris
-		pnpm
-		readline
-		readline70
-		valgrind
-		util-linux
-		
-	];
+  environment.systemPackages = with pkgs; [
+    vim
+    fish
+    clang
+    clang-tools
+    git
+    gnumake
+    ghostty
+    grim
+    iwgtk
+    libbsd
+    libclang
+    libgcc
+    libgccjit
+    lutris
+    pnpm
+    readline
+    readline70
+    rust-analyzer
+    valgrind
+    util-linux
 
-	time.timeZone = "Europe/Paris";
+  ];
 
-	i18n = {
-		extraLocaleSettings = {
-			LC_ADDRESS = "fr_FR.UTF-8";
-			LC_IDENTIFICATION = "fr_FR.UTF-8";
-			LC_MEASUREMENT = "fr_FR.UTF-8";
-			LC_MONETARY = "fr_FR.UTF-8";
-			LC_NAME = "fr_FR.UTF-8";
-			LC_NUMERIC = "fr_FR.UTF-8";
-			LC_PAPER = "fr_FR.UTF-8";
-			LC_TELEPHONE = "fr_FR.UTF-8";
-			LC_TIME = "fr_FR.UTF-8";
-		};
-	};
+  time.timeZone = "Europe/Paris";
 
+  i18n = {
+    extraLocaleSettings = {
+      LC_ADDRESS = "fr_FR.UTF-8";
+      LC_IDENTIFICATION = "fr_FR.UTF-8";
+      LC_MEASUREMENT = "fr_FR.UTF-8";
+      LC_MONETARY = "fr_FR.UTF-8";
+      LC_NAME = "fr_FR.UTF-8";
+      LC_NUMERIC = "fr_FR.UTF-8";
+      LC_PAPER = "fr_FR.UTF-8";
+      LC_TELEPHONE = "fr_FR.UTF-8";
+      LC_TIME = "fr_FR.UTF-8";
+    };
+  };
 
-	security = {
-		rtkit.enable = true;
-		polkit.enable = true;
-		pam = {
-			services = {
-				swaylock = { };
-				hyprlock = { };
-			};
-		};
-	};
+  security = {
+    rtkit.enable = true;
+    polkit.enable = true;
+    pam = {
+      services = {
+        swaylock = { };
+        hyprlock = { };
+      };
+    };
+  };
 
-	fonts.packages = with pkgs; [
-		fira-code
-		fira-code-symbols
-		nerd-fonts.fira-code
-	];
+  fonts.packages = with pkgs; [
+    fira-code
+    fira-code-symbols
+    nerd-fonts.fira-code
+  ];
 }
