8e285363c127e8d82483284316c93902c8d2a642
kix300 <kixwalkiki@gmail.com>
2025-11-05 18:37:39 +0100

    No change lol

--- commons/nixvim/lua/plugins/allplug.lua
+++ commons/nixvim/lua/plugins/allplug.lua
@@ -7,8 +7,18 @@ return {
     },
 
     { "catppuccin/nvim" },
-    { "sainnhe/everforest" },
 
+    {
+      'sainnhe/everforest',
+      lazy = false,
+      priority = 1000,
+      config = function()
+        -- Optionally configure and load the colorscheme
+        -- directly inside the plugin declaration.
+        vim.g.everforest_enable_italic = true
+        vim.cmd.colorscheme('everforest')
+      end
+    },
     { "akinsho/bufferline.nvim" },
 
     {
