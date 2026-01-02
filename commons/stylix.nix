c7dcb6e2ca3dee2a8d7cc69c59adf5a860772440
kix300 <kixwalkiki@gmail.com>
2026-01-02 15:46:02 +0100

    remove stylix to use caelestial scheme

--- commons/stylix.nix
+++ commons/stylix.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}:
 {
 	stylix = {
-		enable = true;
+		enable = false;
 		image = ./srcs/forest.jpg;
 		polarity = "dark";
 		base16Scheme = "${pkgs.base16-schemes}/share/themes/everforest-dark-hard.yaml";
