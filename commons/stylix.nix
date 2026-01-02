32d771cdb91a6ccb19d66a11928b9efab533a4b5
kix300 <kixwalkiki@gmail.com>
2026-01-02 17:03:00 +0100

    stylix removed

--- commons/stylix.nix
+++ commons/stylix.nix
@@ -1,34 +1,31 @@
 {pkgs, ...}:
 {
 	stylix = {
-		enable = true;
-		# image = ./srcs/forest.jpg;
-		# polarity = "dark";
-		# base16Scheme = "${pkgs.base16-schemes}/share/themes/everforest-dark-hard.yaml";
-		# #base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
+		enable = false;
+		image = ./srcs/forest.jpg;
+		polarity = "dark";
 		cursor = {
 			package = pkgs.bibata-cursors;
 			name = "Bibata-Modern-Classic";
 			size = 24;
 		};
-		# opacity.terminal = 0.5;
-		# targets = {
-		# 	gtk.enable = true;
-		# 	xfce.enable = true;
-		# 	wofi.enable = true;
-		# 	firefox.profileNames = [ "default" ];
-		# 	zed.enable = false;
-		# 	nixvim = {
-		# 		enable = true;
-		# 		plugin = "base16-nvim";
-		# 		transparentBackground = {
-		# 			signColumn = true;
-		# 			numberLine = true;
-		# 			main = true;
-		# 		};
-		# 	};
-		# };
-		#
+		targets = {
+			gtk.enable = true;
+			xfce.enable = true;
+			wofi.enable = true;
+			firefox.profileNames = [ "default" ];
+			zed.enable = false;
+			nixvim = {
+				enable = true;
+				plugin = "base16-nvim";
+				transparentBackground = {
+					signColumn = true;
+					numberLine = true;
+					main = true;
+				};
+			};
+		};
+
 	};
 
 	gtk = {
