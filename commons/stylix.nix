fb8fd2e425293f4f5ba89da1f09f3e6ce90a5e19
kix300 <kixwalkiki@gmail.com>
2026-01-02 16:53:14 +0100

    stylix is back for cursor and add gpu recorder and fix hyprland

--- commons/stylix.nix
+++ commons/stylix.nix
@@ -1,34 +1,34 @@
 {pkgs, ...}:
 {
 	stylix = {
-		enable = false;
-		image = ./srcs/forest.jpg;
-		polarity = "dark";
-		base16Scheme = "${pkgs.base16-schemes}/share/themes/everforest-dark-hard.yaml";
-		#base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
+		enable = true;
+		# image = ./srcs/forest.jpg;
+		# polarity = "dark";
+		# base16Scheme = "${pkgs.base16-schemes}/share/themes/everforest-dark-hard.yaml";
+		# #base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
 		cursor = {
 			package = pkgs.bibata-cursors;
 			name = "Bibata-Modern-Classic";
 			size = 24;
 		};
-		opacity.terminal = 0.5;
-		targets = {
-			gtk.enable = true;
-			xfce.enable = true;
-			wofi.enable = true;
-			firefox.profileNames = [ "default" ];
-			zed.enable = false;
-			nixvim = {
-				enable = true;
-				plugin = "base16-nvim";
-				transparentBackground = {
-					signColumn = true;
-					numberLine = true;
-					main = true;
-				};
-			};
-		};
-
+		# opacity.terminal = 0.5;
+		# targets = {
+		# 	gtk.enable = true;
+		# 	xfce.enable = true;
+		# 	wofi.enable = true;
+		# 	firefox.profileNames = [ "default" ];
+		# 	zed.enable = false;
+		# 	nixvim = {
+		# 		enable = true;
+		# 		plugin = "base16-nvim";
+		# 		transparentBackground = {
+		# 			signColumn = true;
+		# 			numberLine = true;
+		# 			main = true;
+		# 		};
+		# 	};
+		# };
+		#
 	};
 
 	gtk = {
