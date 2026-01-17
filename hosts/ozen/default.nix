358d6b8a574e527c75c70d1648646d07eefba40c
kix300 <kixwalkiki@gmail.com>
2026-01-17 11:55:43 +0100

    Add gamescope to steam

--- hosts/ozen/default.nix
+++ hosts/ozen/default.nix
@@ -231,6 +231,11 @@ inputs,
 	};
 	programs.steam = {
 		enable = true;
+		gamescopeSession.enable = true;
+	};
+	programs.gamescope = {
+		enable = true;
+		capSysNice = true;
 	};
 
 	hardware.enableRedistributableFirmware = true;
