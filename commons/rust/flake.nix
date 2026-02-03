edad0fb915eaf93c596c4b2d2ffa3fbe4e27d192
kix300 <kixwalkiki@gmail.com>
2026-02-03 17:21:10 +0100

    Add Flake rust to my conf

--- commons/rust/flake.nix
+++ commons/rust/flake.nix
@@ -10,8 +10,15 @@
 	in {
 		devShells."x86_64-linux".default = pkgs.mkShell {
 			buildInputs = with pkgs; [
+				# Base Pkgs
 				cargo rustc rustfmt clippy rust-analyzer rustlings
+				# Rust lib
+				# Vimjoyer video example
+				# glib
 			];
+
+			nativeBuildInputs = [ pkgs.pkg-config];
+
 			env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 
 		};
