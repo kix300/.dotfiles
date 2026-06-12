e98ebbcfc9590e5510d27b6fdc406584308c0eba
kix300 <kixwalkiki@gmail.com>
2026-06-12 14:15:50 +0200

    feat(qemu): libvirt, qemu, virt-manager

--- hosts/ozen/services.nix
+++ hosts/ozen/services.nix
@@ -1,9 +1,26 @@
 {
 	lib,
+	pkgs,
 	...
 }:
 {
-	virtualisation.libvirtd.enable = true;
+	virtualisation = {
+		libvirtd.enable = true;
+		spiceUSBRedirection.enable = true;
+	};
+	systemd.services.libvirt-default-network = {
+		description = "Start libvirt default network";
+		after = ["libvirtd.service"];
+		wantedBy = ["multi-user.target"];
+		serviceConfig = {
+			Type = "oneshot";
+			RemainAfterExit = true;
+			ExecStart = "${pkgs.libvirt}/bin/virsh net-start default";
+			ExecStop = "${pkgs.libvirt}/bin/virsh net-destroy default";
+			User = "root";
+		};
+	};
+
 	services = {
 		thermald.enable = true;
 		auto-cpufreq = {
