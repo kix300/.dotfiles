a9bf2bdff89319b6727ed4fea45f495b89a7e18b
kix300 <kixwalkiki@gmail.com>
2026-08-05 16:31:35 +0200

    noctalia in hm

--- home/ozen_work/home.nix
+++ home/ozen_work/home.nix
@@ -19,30 +19,6 @@
 	};
 	xdg.configFile."niri/config.kdl".source = niri/config.kdl;
 
-	# wayland.windowManager.hyprland = {
-	# 	enable = true;
-	# 	extraConfig = "
-	# 		${builtins.readFile ./hypr/hyprland/hyprland.conf}
-	# 	";
-	# };
-	# # pour creer un fichier xdg desktop portal hyprland et donc screensharing
-	# # util ? je sais plus on va keep
-	# systemd.user.services.xdg-desktop-portal-hyprland = {
-	# 	Unit = {
-	# 		Description = "XDG Desktop Portal Hyprland";
-	# 		PartOf = [ "graphical-session.target" ];
-	# 		After = [ "graphical-session.target" ];
-	# 	};
-	# 	Service = {
-	# 		Type = "dbus";
-	# 		BusName = "org.freedesktop.impl.portal.desktop.hyprland";
-	# 		ExecStart = "${pkgs.xdg-desktop-portal-hyprland}/libexec/xdg-desktop-portal-hyprland";
-	# 		Restart = "on-failure";
-	# 		RestartSec = 5;
-	# 	};
-	# 	Install = {
-	# 		WantedBy = [ "graphical-session.target" ];
-	# 	};
-	# };
+
 	home.stateVersion = "23.11";
 }
